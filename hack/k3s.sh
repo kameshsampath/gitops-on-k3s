bcee4bb548f3e77c8814f78f2b77839515ae2765
user-01 <user-01@example.com>
2023-01-17 17:22:28 +0530

    skip download of kubectl

--- hack/k3s.sh
+++ hack/k3s.sh
@@ -13,8 +13,3 @@ k3d registry create "${REGISTRY_NAME}" --port "${REGISTRY_PORT}" || true
 
 k3d cluster create -c "$SCRIPT_DIR/k3s-cluster-config.yaml" \
   --registry-use "k3d-${REGISTRY_NAME}:$REGISTRY_PORT"
-
-curl -sSL "https://dl.k8s.io/release/${K3S_VERSION%[+-]*}/bin/$(uname -o | tr '[:upper:]' '[:lower:]')/$(uname -m)/kubectl" -o "$PWD/bin/kubectl"
-
-mkdir -p "$SCRIPT_DIR/../bin"
-chmod +x "$SCRIPT_DIR/../bin/kubectl"
