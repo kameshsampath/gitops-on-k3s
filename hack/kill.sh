68185a4ebb2f8cf9efce9d8d3cea7a2022530967
user-01 <user-01@example.com>
2023-01-17 18:37:16 +0530

    fix registry delete command

--- hack/kill.sh
+++ hack/kill.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 k3d cluster delete "${K3D_CLUSTER_NAME}" || true
 
-k3d registry delete "${REGISTRY_NAME}" --port "${REGISTRY_PORT}" || true
+k3d registry delete "${REGISTRY_NAME}" || true
